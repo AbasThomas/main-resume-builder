1c9f29c479b30565974733e1745338c0367bba54
Thomas Abas <abasienyenethomas2@gmail.com>
2025-05-24 06:23:42 -0800

    task with gsap

--- src/pages/Text.jsx
+++ src/pages/Text.jsx
@@ -1,79 +1,57 @@
-import React from 'react'
-import '../animations/Gsap'
-import gsap from 'gsap'
-import { SplitText } from 'gsap/all'
-const Text = () => {
-gsap.registerPlugin(SplitText)
-let split = SplitText.create('.text', {
-    type : 'words',
-});
-gsap.from(split.words, {
-    y:100,
-    autoAlpha:0,
-    stagger:0.4,
-})
+// // Box.js
+// import React, { useRef, useEffect } from 'react';
+// import { gsap } from 'gsap';
+
+// function Text() {
+
+// useEffect(() => {
+//   gsap.to(".box", {
+//     y: 100,
+//     opacity: 10,
+//     duration: 1,
+//     stagger: 0.2
+//   });
+// }, []);
+
+
+//   return (
+//     <div flex className=" flex justify-center items-center h-screen gap-[20px]">
+//         <div className="box w-[50px] h-[50px] bg-red-500" ></div>
+//         <div className="box w-[50px] h-[50px] bg-red-500" ></div>
+//         <div className="box w-[50px] h-[50px] bg-red-500"></div>
+
+//     </div>
+//   );
+// }
+
+// export default Text;
+
+// task
+
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
+
+function CardList() {
+  useEffect(() => {
+        gsap.fromTo(".card", {
+            opacity : 0,
+            y : -50
+        },
+        {
+            opacity : 1,
+            y : 0,
+            stagger : 0.2,
+        }
+        )}, []);
+
   return (
-    <div className='text flex gap-[5px] text-4xl font-bold justify-center items-center
-    mt-[250px]'>
-        
-        <div className='word flex'>
-                <div className="letter">b</div>
-                <div className="letter">r</div>
-                <div className="letter">e</div>
-                <div className="letter">a</div>
-                <div className="letter">k</div>
-        </div>
-        <div className='word flex'>
-
-            <div className="letter">a</div>
-            <div className="letter">p</div>
-            <div className="letter">a</div>
-            <div className="letter">r</div>
-            <div className="letter">t</div>
-        </div>
-        <div className='word flex'>
-
-            <div className="letter">w</div>
-            <div className="letter">o</div>
-            <div className="letter">r</div>
-            <div className="letter">d</div>
-            <div className="letter">s</div>
-        </div>    
-
-        <div className='word flex'>   
-            <div className="letter">w</div>
-            <div className="letter">i</div>
-            <div className="letter">t</div>
-            <div className="letter">h</div>
-        </div>
-        <div className='word flex'>   
-
-            <div className="letter">g</div>
-            <div className="letter">s</div>
-            <div className="letter">a</div>
-            <div className="letter">p</div>
-        </div>
-        <div className='word flex'>   
-
-            <div className="letter">a</div>
-            <div className="letter">n</div>
-            <div className="letter">d</div>
-        </div>
-        <div className='word flex'>   
-
-            <div className="letter">c</div>
-            <div className="letter">h</div>
-            <div className="letter">a</div>
-            <div className="letter">r</div>
-            <div className="letter">a</div>
-            <div className="letter">c</div>
-            <div className="letter">t</div>
-            <div className="letter">e</div>
-            <div className="letter">r</div>
-        </div>
-
+    <div className="p-10 space-y-4">
+      <div className="card bg-white p-6 rounded shadow">Card 1</div>
+      <div className="card bg-white p-6 rounded shadow">Card 2</div>
+      <div className="card bg-white p-6 rounded shadow">Card 3</div>
+      <div className="card bg-white p-6 rounded shadow">Card 4</div>
     </div>
-  )
+  );
 }
 
-export default Text
+export default CardList;
